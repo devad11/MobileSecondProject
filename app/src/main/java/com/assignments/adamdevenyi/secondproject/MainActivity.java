6efc859a190e21c5abf4f17cc15f95035315c548
Adam Devenyi <adam.devenyi@mycit.ie>
2018-12-05 18:25:27 +0000

    timePicker added, order object working

--- app/src/main/java/com/assignments/adamdevenyi/secondproject/MainActivity.java
+++ app/src/main/java/com/assignments/adamdevenyi/secondproject/MainActivity.java
@@ -1,6 +1,8 @@
 package com.assignments.adamdevenyi.secondproject;
 
+import android.app.TimePickerDialog;
 import android.content.Intent;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ContextMenu;
@@ -10,28 +12,42 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
-    private TextView restaurantTextView;
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener, TimePickerDialog.OnTimeSetListener {
+    private TextView restaurantTextView, restaurantPickTextView;
     private Spinner spinner;
     private String[] restaurants = {"Cork City Center Order", "Douglas Order"};
 
     private static final int SWIPE_MIN_DISTANCE = 120;
     private static final int SWIPE_MAX_OFF_PATH = 250;
     private static final int SWIPE_THRESHOLD_VELOCITY = 200;
+    private String typeText;
     private GestureDetector mDetector;
+    private EditText nameEditText, addressEditText;
+    private TextView deliveryTimeTextView;
+    public Order myOrder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        myOrder = new Order();
+
+        nameEditText = (EditText) findViewById(R.id.nameEditText);
+        addressEditText = (EditText) findViewById(R.id.addressEditText);
         restaurantTextView = (TextView) findViewById(R.id.restaurantTextView);
+        restaurantPickTextView = (TextView) findViewById(R.id.restaurantPickTextView);
+        registerForContextMenu(restaurantPickTextView);
         registerForContextMenu(restaurantTextView);
+        deliveryTimeTextView = (TextView) findViewById(R.id.deliveryTimeTextView);
+        deliveryTimeTextView.setVisibility(deliveryTimeTextView.INVISIBLE);
 
         spinner = (Spinner) findViewById(R.id.spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.options, android.R.layout.simple_spinner_item);
@@ -47,6 +63,14 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                 }
                 if (e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE
                         && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY){
+                    myOrder.setAddress(addressEditText.getText().toString());
+                    myOrder.setName(nameEditText.getText().toString());
+                    myOrder.setShop(restaurantPickTextView.getText().toString());
+                    myOrder.setOrderType(typeText);
+                    if (typeText.equals("Delivery for later")){
+                        myOrder.setDeliveryTime(deliveryTimeTextView.getText().toString());
+                    }
+                    //System.out.println(myOrder.toString());
                     Intent intent = new Intent (MainActivity.this, Toppings.class);
                     startActivity(intent);
                     return true;
@@ -72,9 +96,11 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         switch (item.getItemId()) {
             case R.id.center:
                 Toast.makeText(this, "City Center selected", Toast.LENGTH_SHORT).show();
+                restaurantPickTextView.setText("City Center Pizza");
                 return super.onContextItemSelected(item);
             case R.id.douglas:
                 Toast.makeText(this, "Douglas selected", Toast.LENGTH_SHORT).show();
+                restaurantPickTextView.setText("Douglas Pizza");
                 return super.onContextItemSelected(item);
                 default:
                     return super.onContextItemSelected(item);
@@ -85,11 +111,30 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         String text = parent.getItemAtPosition(position).toString();
+        typeText = text;
+        if (typeText.equals("Delivery for later")){
+            selectTime();
+        }
+        else{
+            deliveryTimeTextView.setVisibility(deliveryTimeTextView.INVISIBLE);
+        }
+
         Toast.makeText(parent.getContext(),text,Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
+    }
+
+    public void selectTime() {
+        DialogFragment timePicker = new TimePickerFragment();
+        timePicker.show(getSupportFragmentManager(), "time picker");
+        deliveryTimeTextView.setVisibility(deliveryTimeTextView.VISIBLE);
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        deliveryTimeTextView.setText("Delivery at " + hourOfDay + ":" + minute);
     }
 }
--- app/src/main/java/com/assignments/adamdevenyi/secondproject/Order.java
+++ app/src/main/java/com/assignments/adamdevenyi/secondproject/Order.java
@@ -8,16 +8,21 @@ import java.util.Map;
 public class Order {
     private String shop;
     private String orderType;
+    private String name;
     private String address;
+    private String deliveryTime;
     private Float price;
     private List<String> pizza;
 
     public Order(){}
 
-    public Order(String shop, String orderType, String address, List<String> pizza){
+    public Order(String shop, String orderType, String name, String address, String deliveryTime, Float price, List<String> pizza){
         this.shop = shop;
         this.orderType = orderType;
+        this.name = name;
         this.address = address;
+        this.deliveryTime = deliveryTime;
+        this.price = price;
         this.pizza = pizza;
 
     }
@@ -38,12 +43,28 @@ public class Order {
         this.orderType = orderType;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getAddress() {
         return address;
     }
 
     public void setAddress(String address) {
         this.address = address;
+    }
+
+    public String getDeliveryTime() {
+        return deliveryTime;
+    }
+
+    public void setDeliveryTime(String deliveryTime) {
+        this.deliveryTime = deliveryTime;
     }
 
     public Float getPrice() {
@@ -60,6 +81,19 @@ public class Order {
 
     public void setPizza(List<String> pizza) {
         this.pizza = pizza;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "shop='" + shop + '\'' +
+                ", orderType='" + orderType + '\'' +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", deliveryTime='" + deliveryTime + '\'' +
+                ", price=" + price +
+                ", pizza=" + pizza +
+                '}';
     }
 }
 
