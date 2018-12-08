3cbe94ac3150501c453c6c72fa90db7bbb5a8bf3
Adam Devenyi <adam.devenyi@mycit.ie>
2018-12-08 04:57:01 +0000

    notifications dont work

--- app/src/main/java/com/assignments/adamdevenyi/secondproject/Confirmation.java
+++ app/src/main/java/com/assignments/adamdevenyi/secondproject/Confirmation.java
@@ -1,10 +1,10 @@
 package com.assignments.adamdevenyi.secondproject;
 
-import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import java.text.DateFormat;
@@ -13,13 +13,16 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
+import static com.assignments.adamdevenyi.secondproject.CreateChannel.CHANNEL_1_ID;
+
+
 public class Confirmation extends AppCompatActivity {
 
     private TextView phoneChoicetextView, addressChoicetextView, nameChoicetextView, priceChoicetextView, timeChoicetextView, orderTypeChoiceTextView, shopChoicetextView;
     private Order myOrder;
     private String currentTime;
     private Calendar calendar;
-    private Button orderButton;
+    private NotificationManagerCompat notificationManager;
 
 
     @Override
@@ -55,26 +58,33 @@ public class Confirmation extends AppCompatActivity {
         priceChoicetextView.setText(String.format("%.2f", myOrder.getPrice()));
         orderTypeChoiceTextView.setText(myOrder.getOrderType().toString());
         shopChoicetextView.setText(myOrder.getShop().toString());
-        if (myOrder.getOrderType().equals("Collect")){
+        if (myOrder.getOrderType().equals("Collect")) {
             timeChoicetextView.setText(currentTime);
-        }
-        else if(myOrder.getOrderType().equals("ASAP Delivery")){
+        } else if (myOrder.getOrderType().equals("ASAP Delivery")) {
             calendar.add(Calendar.MINUTE, 20);
             currentTime = df.format(calendar.getTime());
             timeChoicetextView.setText(currentTime);
-        }
-        else{
+        } else {
             timeChoicetextView.setText(myOrder.getDeliveryTime().toString());
         }
 
-        orderButton = (Button) findViewById(R.id.orderButton);
-        orderButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Confirmation.this, PizzaArrived.class);
-                startActivity(intent);
-            }
-        });
+        notificationManager = NotificationManagerCompat.from(this);
 
     }
+        public void sendNotification(View v){
+        System.out.println("WWWWHHHHYYYYYYYYY");
+            String title = "Best Pizza";      //gets title from user
+            String message = "Thank you for your order";  //gets message form user
+
+            NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, CHANNEL_1_ID)
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setContentTitle(title)
+                    .setContentText(message)
+                    .setPriority(NotificationCompat.PRIORITY_MAX);
+
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+// notificationId is a unique int for each notification that you must define
+            notificationManager.notify(1, mBuilder.build());
+        }
 }
